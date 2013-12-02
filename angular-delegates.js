b0d5b5dc69e905afc73120376c24421296b716e8
doly mood <dolymood@gmail.com>
2013-12-02 20:43:41 +0800

    Update angular-delegates.js

--- angular-delegates.js
+++ angular-delegates.js
@@ -1,2 +1,52 @@
 'use strict'
 
+;(function() {
+	
+	var root = document.documentElement;
+	var matchesSelector = root.matches || root.matchesSelector ||
+												root.webkitMatchesSelector || root.mozMatchesSelector ||
+												root.msMatchesSelector || root.oMatchesSelector;
+	var mSelector = function(ele, selector) {
+		if (matchesSelector) {
+			return matchesSelector.call(ele, selector);
+		}
+		return ele.tagName.toLowerCase() === selector;
+	};
+	var getClosest = function(ele, selector, rootE) {
+		rootE || (rootE = root);
+		while(ele && ele != rootE && (rootE.contains(ele))) {
+			if (mSelector(ele, selector)) {
+				return ele;
+			} else {
+				ele = ele.parentNode;
+			}
+		}
+		return null;
+	};
+	
+	if (!root.contains) {
+		Node.prototype.contains = function(arg) {
+				return !!(this.compareDocumentPosition(arg) & 16);
+		}
+	}
+	
+	angular.module('DelegateEvents', []).directive(function($parse) {
+		
+		return function(scope, ele, attrs) {
+			var selector = attrs.selector;
+			var eventName = attrs.eventName || 'click';
+			var func = $parse(attrs.eventDelagation);
+			ele.on(eventName, function(e) {
+				var target = e.target;
+				var el;
+				if ((el = getClosest(target, selector, ele[0]))) {
+					e.delegationTarget = el;
+					scope.$apply(function() {
+							func(ag.element(el).scope(), {$event: e});
+					});
+				}
+			});
+		};
+	});
+
+})()
